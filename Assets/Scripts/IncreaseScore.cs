4b6199b17c08ccd3e73110686e2678df44719aed
Riley <rileygauchier@gmail.com>
2020-05-21 20:35:10 -0700

    added scoring

---
+++ Assets/Scripts/IncreaseScore.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IncreaseScore : MonoBehaviour
+{
+    public Text myScore;
+
+    [SerializeField]
+    private int score = 0;
+    void Awake()
+    {
+        myScore.text = "Score: 0";
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        score++;
+        myScore.text = "Score: " + score;
+    }
+}
